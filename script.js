3e31ccd0768b86fe84c8d1e8041bdf1d139d8f5e
Boialex <xdr007@gmail.com>
2016-11-24 23:37:03 +0300

    add fun JS

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     $('body').animate({"background-color": my_color});
     $('.block').addClass('.invisible').hide();
     $('table').css({font: "16px"});
+    $('p, tr, .block h3').css({"border-radius": "10px"});
     var clicked = 0;
     $('h3').click(function() {
         var tag = $(this).next();
@@ -23,5 +24,46 @@ $(document).ready(function() {
     });
     $('p, tr, .block h3').mouseleave(function() {
         $(this).delay(200).animate({"background-color" : my_color, "font-size" : "-=3"}, 300);
-    })
+    });
+    var newdiv =$('<div id="img"><img src="download.jpg"></div>');
+    $('.cv').append(newdiv);
+    var t = $('<p>You can move me with arrow buttons</p>');
+    newdiv.prepend(t);
+    newdiv.children('p').hide();
+    $('img').css({width : '10%', height: '10%'});
+    $('img').animate({"border-radius": "100%"}, 500);
+    newdiv.css({'text-align':'center'});
+    $('img').click(function() { 
+        var pos = Math.floor((Math.random() * 100) - 50);
+        var shift = '+=' + pos.toString() + 'px';
+        // alert(shift);
+        $(this).effect('bounce', {times:3}, 100).delay(100).animate({"margin-left": shift}, 'fast');
+        newdiv.children('p').show();
+        // newdiv.children('p').delay(500).hide();
+    });
+    $(document).keydown(function(key) {
+        newdiv.children('p').hide();
+        var how_far = "40px";
+        switch(parseInt(key.which,10)) {
+			// Left arrow key pressed
+			case 37:
+				$('img').animate({"margin-left": "-="+how_far}, 'fast');
+				break;
+			// Up Arrow Pressed
+			case 38:
+			    $('img').animate({"margin-top": "-="+how_far}, 'fast');
+				// Put our code here
+				break;
+			// Right Arrow Pressed
+			case 39:
+			    $('img').animate({"margin-left": "+="+how_far}, 'fast');
+				// Put our code here
+				break;
+			// Down Arrow Pressed
+			case 40:
+			    $('img').animate({"margin-top": "+="+how_far}, 'fast');
+				// Put our code here
+				break;
+		}
+	});
 });
